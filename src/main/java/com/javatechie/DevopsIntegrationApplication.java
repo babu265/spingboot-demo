efc33366f71fdffb1027d595d2cbe241ae3dadb6
Ramesh Dasari <129903569+babu265@users.noreply.github.com>
2024-03-26 18:57:19 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "Demo for CHEKK";
 	}
 
 	public static void main(String[] args) {
